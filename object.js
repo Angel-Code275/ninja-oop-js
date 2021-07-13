a72fa9f0e05fc220f521899a85a88c13ccc920e7
BJ-stack-art <bj.angel119@gmail.com>
2021-07-13 22:20:13 +0700

    initialize this directory become repository git

---
+++ object.js
@@ -0,0 +1,40 @@
+function User(email, name) {
+    this.email = email;
+    this.name = name;
+    this.online = false;
+}
+
+
+User.prototype.login = function() {
+    this.online = true;
+    console.log(`${this.email} has logged in`);
+}
+
+User.prototype.logout = function() {
+    this.online = false;
+    console.log(`${this.email} has logged out`);
+}
+
+
+
+function Admin(...args) {
+    User.apply(this, args);
+    this.role = 'super admin';
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(u) {
+    users = users.filter(user => {
+        return user.email != u.email;
+    })
+}
+
+
+let userOne = new User('ryu@ninjas.com' , 'Ryu');
+let userTwo = new User('yoshi@mariokorp.com' , 'Yoshi');
+let admin = new Admin('shaun@ninjas.com' , 'Shaun');
+
+let users = [userOne, userTwo, admin];
+
+console.log(admin);
